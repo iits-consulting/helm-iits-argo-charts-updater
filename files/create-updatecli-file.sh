80a471fc028fa1c31258b0a858f3a47b81ff723a
Victor Getz <sd>
2023-07-31 13:23:29 +0200

    Update updatecli config

--- files/create-updatecli-file.sh
+++ files/create-updatecli-file.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-## Add the repository (replace with your own URL if needed)
-#helm repo add iits https://charts.iits.tech
-#
-## Update the repository to fetch the latest chart information
-#helm repo update
-
 mkdir -p updatecli
 
 # Get the chart names from the repository
@@ -18,52 +12,39 @@ if [[ ! -f "$VALUES_FILE" ]]; then
   exit 1
 fi
 
-# Iterate through the chart names
-chart_names=""
-for chart in $(yq e '.charts | keys | .[]' $VALUES_FILE); do
-  # Check if path is set
-  path=$(yq e ".charts.$chart.path // \"null\"" $VALUES_FILE)
+# Iterate through the chart names in each values file
+yq e '.charts | keys | .[]' "$VALUES_FILE" | while read -r release_name; do
+  path=$(yq e ".charts.$release_name.path // \"null\"" "$VALUES_FILE")
   if [ "$path" = "null" ]; then
-    # Check if overrideChartName is set
-    override_name=$(yq e ".charts.$chart.overrideChartName // \"null\"" $VALUES_FILE)
-    if [ "$override_name" != "null" ]; then
-      chart_names+="$override_name "
-    else
-      chart_names+="$chart "
-    fi
-  fi
-done
+    overrideChartName=$(yq e ".charts.$release_name.overrideChartName // \"$release_name\"" "$VALUES_FILE")
 
-echo "$chart_names"
-
-# Loop through the chart names and create a yaml file for each
-for chart_name in $chart_names; do
-  # Create a yaml file with the chart name
-  cat > updatecli/"$chart_name.yaml" <<EOL
+    # Create a yaml file with the chart name
+    cat > "updatecli/$release_name.yaml" <<EOL
 sources:
   latestRelease:
     kind: helmChart
     spec:
       url: "https://charts.iits.tech"
-      name: "$chart_name"
+      name: "$overrideChartName"
       version: "*.*.*"
 conditions:
   chartExists:
-    name: "iits-consulting $chart_name Helm Chart is used"
+    name: "iits-consulting $release_name Helm Chart is used"
     kind: yaml
     disablesourceinput: true
     spec:
-      file: "values.yaml"
-      key: "charts.$chart_name"
+      file: "$VALUES_FILE"
+      key: "charts.$release_name"
       keyonly: true
 targets:
   chartVersion:
-    name: Bump iits-consulting $chart_name
+    name: "Bump iits-consulting $release_name"
     kind: yaml
     spec:
-      file: "values.yaml"
-      key: "charts.$chart_name.targetRevision"
+      file: "$VALUES_FILE"
+      key: "charts.$release_name.targetRevision"
       versionIncrement: "patch"
 EOL
-  echo "Created YAML file for $chart_name"
+    echo "Created YAML file for $release_name in $VALUES_FILE"
+  fi
 done
