53c72efe1d2aaf5c7e7314b0c3d8ec545c2986e6
Victor Getz <sd>
2024-02-02 13:15:50 +0100

    feature(tests): add pre-commit hook which checks if chart is valid

--- run.sh
+++ run.sh
@@ -8,6 +8,4 @@ rm ./create-updatecli-file.sh
 #Add git hooks
 IITS_CHART_NAME=$(basename "$PWD")
 
-sed "s/IITS_CHART_NAME/${IITS_CHART_NAME}/g" $HELM_PLUGIN_DIR/git-hooks/pre-commit > $HELM_PLUGIN_DIR/git-hooks/temp_pre_commit
-mv $HELM_PLUGIN_DIR/git-hooks/temp_pre_commit ../.git/hooks/pre-commit
-chmod +x ../.git/hooks/pre-commit
+git config core.hooksPath $IITS_CHART_NAME/git-hooks
